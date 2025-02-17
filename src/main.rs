6a88b62b725473feefac19313f7d7030ce205f21
jnt87 <jnthomas8@gmail.com>
2025-02-16 23:34:34 -0500

    signal handled in thread avoiding blocking calls

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use io_uring::{IoUring, opcode, types};
-use std::{ffi::CString, process};
+use std::{
+    ffi::CString,
+    process,
+    sync::{Arc, atomic::{AtomicBool, Ordering}},
+    thread,
+    time::Duration,
+};
+use signal_hook::iterator::Signals;
 
 macro_rules! trust_me_bro {
     ($($stmt:stmt;)*) => {
@@ -9,7 +16,23 @@ macro_rules! trust_me_bro {
     };
 }
 
+fn handle_signals(signals: Vec<i32>, running: Arc<AtomicBool>) {
+    let mut signals = Signals::new(&signals).expect("Failed to register signals");
+
+    thread::spawn(move || {
+        for signal in signals.forever() {
+            println!("Recieved signal: {}", signal);
+            running.store(false, Ordering::Relaxed);
+            break;
+        }
+    });
+
+}
+
 fn main() {
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
     let args: Vec<String> = std::env::args().collect();
     if args.len() <2 {
         eprintln!("Usage: {} <file>", args[0]);
@@ -30,17 +53,34 @@ fn main() {
         let _ = sq.push(&entry).expect("Failed to push operation");
     };
 
-    ring.submit_and_wait(1).expect("Failed to submit");
+    let running = Arc::new(AtomicBool::new(true));
+    let signals = vec![libc::SIGINT, libc::SIGTERM, libc::SIGHUP];
 
-    let mut cq = ring.completion();
-
-    if let Some(cqe) = cq.next() {
-        if cqe.result() < 0 {
-            eprintln!("Error removing file: {}", std::io::Error::from_raw_os_error(-cqe.result()));
-            process::exit(1);
-        } else {
-            println!("Successfully removed: {}", path);
+    handle_signals(signals, running.clone());
+    
+    while running.load(Ordering::Relaxed) {
+        match ring.submit_and_wait(1) {
+            Ok(_) => {
+                let mut cq = ring.completion();
+                if let Some(cqe) = cq.next() {
+                    if cqe.result() < 0 {
+                        eprintln!("Error removing file: {}", std::io::Error::from_raw_os_error(-cqe.result()));
+                        process::exit(1);
+                    } else {
+                        println!("Successfully removed: {}", path);
+                    }
+                }
+                break;
+            }
+            Err(e) if running.load(Ordering::Relaxed) => {
+                eprintln!("Error waiting for completion: {}", e);
+            }
+            _ => break,
         }
     }
-
+ 
+    if !running.load(Ordering::Relaxed) {
+        eprintln!("\nOperation interrupted. Exiting.");
+        process::exit(130);
+    }
 }
